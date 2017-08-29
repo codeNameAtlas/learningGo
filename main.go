b941d3328d44bc66ab22f1067e2078aa9ef55dee
codeNameAtlas <alex.navarrete@segment.com>
2017-08-28 23:03:32 -0700

    render real data from classify api

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
   "fmt"
   "net/http"
   "html/template"
+
   "database/sql"
   _ "github.com/mattn/go-sqlite3"
+
   "encoding/json"
+  "net/url"
+  "io/ioutil"
+  "encoding/xml"
 )
 
 type Page struct {
@@ -17,10 +22,10 @@ type Page struct {
 //create a Struct for outgoing results
 
 type SearchResult struct {
-  Title string
-  Author string
-  Year string
-  ID string
+  Title string `xml:"title,attr"`
+  Author string `xml:"author,attr"`
+  Year string `xml:"hyr,attr"`
+  ID string `xml:"owi,attr"`
 }
 
 func main() {
@@ -41,11 +46,13 @@ func main() {
   })
 
   http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-    results := []SearchResult{
-      SearchResult{"Moby-Dick", "Herman Melville", "1851", "222222"},
-      SearchResult{"The Adventures of Huckleberry Finn", "Mark Twain", "1884", "444444"},
-      SearchResult{"The Catcher in the Rye", "JD Salinger", "1951", "333333"},
+    var results []SearchResult
+    var err error
+
+    if results, err = search(r.FormValue("search")); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
     }
+
     encoder := json.NewEncoder(w)
     if err := encoder.Encode(results); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -53,7 +60,36 @@ func main() {
 
   })
 
-
-
   fmt.Println(http.ListenAndServe(":8080", nil))
+
+}
+
+type ClassifySearchResponse struct {
+  Results []SearchResult `xml:"works>work"`
+
+}
+
+//creating a new function that takes in search results and returns and error
+//define two variables: one for results one for errors
+func search(query string) ([]SearchResult, error) {
+
+  var resp *http.Response
+  var err error
+
+  if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err != nil {
+    //if the error is not equal to nil
+    return []SearchResult{}, err
+  }
+
+  //read the body of the response
+  defer resp.Body.Close()
+
+  var body []byte
+  if body, err = ioutil.ReadAll(resp.Body); err != nil {
+    return []SearchResult{}, err
+  }
+
+  var c ClassifySearchResponse
+  err = xml.Unmarshal(body, &c)
+  return c.Results, err
 }
